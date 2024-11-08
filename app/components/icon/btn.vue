5d456936bb4edbf798a3c0a658c61607bc2aeb19
Chris Searle <chris@chrissearle.org>
2024-11-08 08:49:42 +0100

    Fixes #5 - basic filters

---
+++ app/components/icon/btn.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {Icon} from "@iconify/vue";
+
+const props = defineProps<{
+  title: string,
+  icon: string
+}>()
+
+const emit = defineEmits<{
+  select: [value: string]
+}>()
+
+const click = (() => {
+  emit('select', props.title)
+})
+</script>
+
+<template>
+      <v-btn @click="click" class="w-100">
+        <template v-slot:prepend>
+          <Icon :icon="props.icon" />
+        </template>
+        <template v-slot:default>
+          {{ props.title }}
+        </template>
+      </v-btn>
+</template>
--- app/pages/conference/[conferenceId]/index.vue
+++ app/pages/conference/[conferenceId]/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import {Icon} from "@iconify/vue";
+
 const route = useRoute()
 
-const { findConference, conferenceTitle } = useConferences()
-const { sessionLink } = useSessions()
+const {findConference, conferenceTitle} = useConferences()
+const {sessionLink} = useSessions()
 
 const {
   data: conferences,
@@ -33,14 +35,115 @@ const conference = computed(() => {
   }
 })
 
+const ready = computed(() => {
+  return status.value === 'success' && conferenceStatus.value === 'success';
+})
+
+const selectedFormat = ref<string | undefined>()
+const selectedLanguage = ref<string | undefined>()
+const selectedStatus = ref<string | undefined>()
+
+const selectFormat = ((format: string) => {
+  selectedFormat.value = format;
+})
+
+const selectStatus = ((status: string) => {
+  selectedStatus.value = status;
+})
+
+const selectLanguage = ((language: string) => {
+  selectedLanguage.value = language;
+})
+
+const clearFormat = (() => {
+  selectedFormat.value = undefined;
+})
+
+const clearStatus = (() => {
+  selectedStatus.value = undefined;
+})
+
+
+const clearLanguage = (() => {
+  selectedLanguage.value = undefined;
+})
+
+const filteredSessions = computed(() => {
+  return sessions.value?.filter((session) => {
+    return selectedFormat.value === undefined || session.format === selectedFormat.value.toLocaleUpperCase();
+  }).filter((session) => {
+    return selectedStatus.value === undefined || session.status === selectedStatus.value.toLocaleUpperCase();
+  }).filter((session) => {
+    return selectedLanguage.value === undefined || session.language === selectedLanguage.value.toLocaleUpperCase();
+  });
+})
 </script>
 
 <template>
   <v-container>
 
-    <h1 v-if="conference">{{ conferenceTitle(conference) }}</h1>
+    <h1 v-if="ready">{{ conferenceTitle(conference) }}</h1>
+
+    <div v-if="ready" class="mx-2">
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            <b>Filters:</b>
+            <span class="mx-2" v-if="selectedFormat">Format: {{ selectedFormat }}</span>
+            <span class="mx-2" v-if="selectedStatus">Status: {{ selectedStatus }}</span>
+            <span class="mx-2" v-if="selectedLanguage">Language: {{ selectedLanguage }}</span>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <div class="d-flex ga-3 justify-center">
+              <v-card class="w-25 d-flex flex-column">
+                <v-card-title>Format</v-card-title>
+                <v-card-text>
+                  <div class="d-flex flex-column ga-3">
+                    <IconBtn title="Presentation" icon="carbon:group-presentation" @select="selectFormat"/>
+                    <IconBtn title="Lightning Talk" icon="carbon:lightning" @select="selectFormat"/>
+                    <IconBtn title="Workshop" icon="carbon:laptop" @select="selectFormat"/>
+                    <IconBtn title="Panel Debate" icon="carbon:forum" @select="selectFormat"/>
+                  </div>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn @click="clearFormat">Clear</v-btn>
+                </v-card-actions>
+              </v-card>
+              <v-card class="w-25 d-flex flex-column">
+                <v-card-title>Status</v-card-title>
+                <v-card-text>
+                  <div class="d-flex flex-column ga-3">
+                    <IconBtn title="Approved" icon="carbon:task-approved" @select="selectStatus"/>
+                    <IconBtn title="Submitted" icon="carbon:task-view" @select="selectStatus"/>
+                    <IconBtn title="Rejected" icon="carbon:task-remove" @select="selectStatus"/>
+                    <IconBtn title="Historic" icon="carbon:task-asset-view" @select="selectStatus"/>
+                    <IconBtn title="Draft" icon="carbon:task-add" @select="selectStatus"/>
+                  </div>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn @click="clearStatus">Clear</v-btn>
+                </v-card-actions>
+              </v-card>
+              <v-card class="w-25 d-flex flex-column">
+                <v-card-title>Language</v-card-title>
+                <v-card-text>
+                  <div class="d-flex flex-column ga-3">
+                    <IconBtn title="English" icon="openmoji:flag-united-kingdom" @select="selectLanguage"/>
+                    <IconBtn title="Norwegian" icon="openmoji:flag-norway" @select="selectLanguage"/>
+                  </div>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn @click="clearLanguage">Clear</v-btn>
+                </v-card-actions>
+              </v-card>
+            </div>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
 
     <v-text-field
+        v-if="ready"
         v-model="search"
         label="Search"
         prepend-inner-icon="mdi-magnify"
@@ -54,39 +157,47 @@ const conference = computed(() => {
         color="primary"
         indeterminate
         class="ma-16"
-        v-if="status === 'pending'"
+        v-if="!ready"
     />
 
     <v-data-table
-        v-if="status === 'success' && conferenceStatus === 'success'"
-        :items="sessions"
+        v-if="ready"
+        :items="filteredSessions"
         :headers="headers"
-        density="compact"
+        density="comfortable"
         :search="search"
         items-per-page="100">
       <template v-slot:[`item.speakers`]="{ value }">
-        <v-list lines="two" density="compact">
+        <v-list lines="two" density="comfortable">
           <v-list-item v-for="(speaker, index) in value" :idx="index" :title="speaker.name" :subtitle="speaker.email"/>
         </v-list>
       </template>
       <template v-slot:[`item.location`]="{ value }">
-        <v-list lines="two" density="compact">
+        <v-list lines="two" density="comfortable">
           <v-list-item v-for="(speaker, index) in value" :idx="index" :title="speaker.postcode"
                        :subtitle="speaker.location"/>
         </v-list>
       </template>
       <template v-slot:[`item.id`]="{ value }">
-        <v-btn v-if="value" :to="sessionLink(conference, value)"><v-icon>mdi-file-document-outline</v-icon></v-btn>
+        <v-btn v-if="value" :to="sessionLink(conference, value)">
+          <v-icon>mdi-file-document-outline</v-icon>
+        </v-btn>
       </template>
       <template v-slot:[`item.status`]="{ value }">
-        <IconStatus :status="value" />
+        <IconStatus :status="value"/>
       </template>
       <template v-slot:[`item.format`]="{ value }">
-        <IconFormat :format="value" />
+        <IconFormat :format="value"/>
       </template>
       <template v-slot:[`item.language`]="{ value }">
-        <IconLanguage :language="value" />
+        <IconLanguage :language="value"/>
       </template>
     </v-data-table>
   </v-container>
 </template>
+
+<style scoped>
+.filter {
+  flex-direction: column;
+}
+</style>
